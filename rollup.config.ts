074d54a3a48f892cda72b7b24116df6065535431
nanxiaobei <nanxiaobei@gmail.com>
2023-09-16 23:34:32 +0800

    refine rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -12,8 +12,7 @@ const cjsOutput = { file: pkg.main, format: 'cjs', exports: 'auto' } as const;
 const esmOutput = { file: pkg.module, format: 'es' } as const;
 const dtsOutput = { file: pkg.types, format: 'es' } as const;
 
-const tsPlugin = typescript();
-const postcssPlugin = postcss({ minimize: true });
+const plugins = [typescript(), postcss({ minimize: true })];
 
 const external = [
   ...Object.keys({ ...pkg.dependencies, ...pkg.peerDependencies }),
@@ -21,8 +20,8 @@ const external = [
 ];
 
 const config: RollupOptions[] = [
-  { input, output: cjsOutput, plugins: [tsPlugin, postcssPlugin], external },
-  { input, output: esmOutput, plugins: [tsPlugin, postcssPlugin], external },
+  { input, output: cjsOutput, plugins, external },
+  { input, output: esmOutput, plugins, external },
   { input, output: dtsOutput, plugins: [dts()], external: [/\.scss$/] },
 ];
 
