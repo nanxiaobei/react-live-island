2a961c3c75d5efcb6be7d103bbb6f548cb7c26d1
nanxiaobei <nanxiaobei@gmail.com>
2023-09-17 04:24:49 +0800

    update rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,7 +3,6 @@ import { readFileSync } from 'fs';
 import type { RollupOptions } from 'rollup';
 import dts from 'rollup-plugin-dts';
 import postcss from 'rollup-plugin-postcss';
-import update from './rollup-plugin-update';
 
 const pkg = JSON.parse(readFileSync('./package.json') as unknown as string);
 
@@ -13,17 +12,7 @@ const cjsOutput = { file: pkg.main, format: 'cjs', exports: 'auto' } as const;
 const esmOutput = { file: pkg.module, format: 'es' } as const;
 const dtsOutput = { file: pkg.types, format: 'es' } as const;
 
-const plugins = (type: 'cjs' | 'esm') => [
-  typescript(),
-  postcss({ extract: true }),
-  update((code) => {
-    const cssPos = '// css';
-    const cssFile = `'./react-live-island.${type}.css'`;
-    return type === 'cjs'
-      ? code.replace(cssPos, `require(${cssFile});\n`)
-      : code.replace(cssPos, `import ${cssFile};\n`);
-  }),
-];
+const plugins = [typescript(), postcss({ minimize: true })];
 
 const external = [
   ...Object.keys({ ...pkg.dependencies, ...pkg.peerDependencies }),
@@ -31,8 +20,8 @@ const external = [
 ];
 
 const config: RollupOptions[] = [
-  { input, output: cjsOutput, plugins: plugins('cjs'), external },
-  { input, output: esmOutput, plugins: plugins('esm'), external },
+  { input, output: cjsOutput, plugins, external },
+  { input, output: esmOutput, plugins, external },
   { input, output: dtsOutput, plugins: [dts()], external: [/\.scss$/] },
 ];
 
